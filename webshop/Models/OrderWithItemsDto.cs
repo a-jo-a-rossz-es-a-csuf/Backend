cae6d74f712aab5184036cf69209358e4caf0f23
babinszkib <145444728+babinszkib@users.noreply.github.com>
2026-04-15 11:45:05 +0200

    Add files via upload

---
+++ webshop/Models/OrderWithItemsDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace webshop.Models;
+
+public class OrderWithItemsDto
+{
+    public Rendelesek? Order { get; set; }
+    public List<RendelesTetelek>? Tetelek { get; set; }
+}
