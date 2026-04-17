2b97b7336706d959decdc72cfad7e6da67895bba
myusernameisinvalid <90838645+myusernameisinvalid@users.noreply.github.com>
2026-04-17 21:18:22 +0200

    Update OrderWithItemsDto.cs

--- webshop/Models/OrderWithItemsDto.cs
+++ webshop/Models/OrderWithItemsDto.cs
@@ -1,9 +1,20 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace webshop.Models;
 
 public class OrderWithItemsDto
 {
     public Rendelesek? Order { get; set; }
-    public List<RendelesTetelek>? Tetelek { get; set; }
+
+    public List<TetelDto>? Tetelek { get; set; }
+}
+
+public class TetelDto
+{
+    public int? AlkatreszId { get; set; }
+    public int? OlajId { get; set; }
+    public string? TermekNev { get; set; }
+    public int Mennyiseg { get; set; }
+    public decimal Egysegar { get; set; }
+    public decimal Osszeg { get; set; }
 }
